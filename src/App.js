5ecd22874b7e288b707ed0eebeab5cae24751e85
Julio César Noguera <jnoguera@smarttaxi.co>
2020-10-12 17:28:32 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import ReactGa from 'react-ga';
+
 /* import pages */
 import Home from './pages/Home';
 import Privacity from './pages/Privacity';
@@ -12,6 +13,13 @@ import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  useEffect(() =>{
+    ReactGa.initialize('UA-180445945-1')
+
+    //to report page view
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
   return (
     <>
       <div className="container">
